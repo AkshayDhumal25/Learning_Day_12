6724358d46f555b5a91b31fda883069ca5e8f1ad
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-31 10:10:09 +0530

    Swap Values without using any variable a=10, b=12.

--- Program.cs
+++ Program.cs
@@ -79,6 +79,35 @@
 // 1. WAP to find Second largest element in an array : arr[] = [12, 35, 1, 10, 34, 1, 35], without
 // sorting, without using any built-in methods and without deleting duplicate elements. What will be
 // the time complexity?
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int[] array = { 12, 35, 1, 10, 34, 1, 35 };
+//            int secondLarge = 0;
+//            int large = 0;
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                if (array[i] > large)
+//                {
+//                    secondLarge = large;
+//                    large = array[i];
+//                }
+//                if (array[i] < large && array[i] > secondLarge)
+//                {
+//                    secondLarge = array[i];
+//                }
+//            }
+//            Console.WriteLine($" Second Largest Element from array : {secondLarge}");
+//        }
+//    }
+//}
+
+
+// 2. Swap Values without using any variable a=10, b=12.
 using System;
 namespace logicals
 {
@@ -86,22 +115,14 @@ namespace logicals
     {
         public static void Main(string[] args)
         {
-            int[] array = { 12, 35, 1, 10, 34, 1, 35 };
-            int secondLarge = 0;
-            int large = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] > large)
-                {
-                    secondLarge = large;
-                    large = array[i];
-                }
-                if (array[i] < large && array[i] > secondLarge)
-                {
-                    secondLarge = array[i];
-                }
-            }
-            Console.WriteLine($" Second Largest Element from array : {secondLarge}");
+            int a = 10;
+            int b = 12;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"A : {a} B : {b}");
         }
     }
 }
