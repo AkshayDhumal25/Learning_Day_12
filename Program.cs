b396a3910929a3d676b6a7624ae39d8504028da4
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-31 11:54:29 +0530

    WAP for getting a square root of a given number.

--- Program.cs
+++ Program.cs
@@ -346,37 +346,53 @@
 
 
 // 11. Find prime numbers from 1 ....n
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter the number to get the prime numbers : ");
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            for (int i = 3; i < n; i++)
+//            {
+//                if (isPrime(i))
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
+//        }
+//        public static bool isPrime(int n)
+//        {
+//            if (n <= 1) return false;
+//            if (n == 2) return true;
+//            if (n % 2 == 0) return false;
+
+//            for (int i = 3; i * i < n; i += 2)
+//            {
+//                if (n % i == 0)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//    }
+//}
+
+// ]12. WAP for getting a square root of a given number.
 using System;
 namespace logicals
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] str)
         {
-            Console.WriteLine("Enter the number to get the prime numbers : ");
+            Console.WriteLine("Enter the number to get the sqrt : ");
             int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 3; i < n; i++)
-            {
-                if (isPrime(i))
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
-        public static bool isPrime(int n)
-        {
-            if (n <= 1) return false;
-            if (n == 2) return true;
-            if (n % 2 == 0) return false;
-
-            for (int i = 3; i * i < n; i += 2)
-            {
-                if (n % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
+            var res = Math.Sqrt(n);
+            Console.WriteLine(res);
         }
     }
 }
