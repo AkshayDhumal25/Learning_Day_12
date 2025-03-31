91bf601ced04579a324278b03f11791182332778
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-31 11:46:03 +0530

    Find prime numbers from 1 ....n

--- Program.cs
+++ Program.cs
@@ -324,22 +324,59 @@
 
 
 // 10. Code to find even numbers in a list
+//using System;
+//using System.Collections.Generic;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//            foreach (var i in list)
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+// 11. Find prime numbers from 1 ....n
 using System;
-using System.Collections.Generic;
 namespace logicals
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            foreach (var i in list)
+            Console.WriteLine("Enter the number to get the prime numbers : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 3; i < n; i++)
             {
-                if (i % 2 == 0)
+                if (isPrime(i))
                 {
                     Console.WriteLine(i);
                 }
             }
+        }
+        public static bool isPrime(int n)
+        {
+            if (n <= 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+
+            for (int i = 3; i * i < n; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
