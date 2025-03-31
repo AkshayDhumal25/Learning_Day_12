5d033c9fc31183f9be7e1183993d279c0df4e8b5
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-31 09:19:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Diamond Problem
+using System;
+namespace diamondProblem
+{
+    interface IA
+    {
+        public void Print()
+        {
+            Console.WriteLine("IA");
+        }
+    }
+    interface IB : IA { }
+    interface IC : IA { }
+    class D : IB, IC
+    {
+        public void Print()
+        {
+            Console.WriteLine("Class D"); ;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            D d = new D();
+            d.Print();
+        }
+    }
+}
